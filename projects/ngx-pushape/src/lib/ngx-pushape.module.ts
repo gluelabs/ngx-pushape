afe7f8461ed73d1edd7045e993e42fdb7c6766b6
Paolo Tesser <p.tesser921@gmail.com>
2020-07-14 14:21:42 +0200

    chore: remove unused empty component in the lib

--- projects/ngx-pushape/src/lib/ngx-pushape.module.ts
+++ projects/ngx-pushape/src/lib/ngx-pushape.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { NgxPushapeComponent } from './ngx-pushape.component';
-
-
 
 @NgModule({
-  declarations: [NgxPushapeComponent],
+  declarations: [],
   imports: [
   ],
-  exports: [NgxPushapeComponent]
+  exports: []
 })
 export class NgxPushapeModule { }
